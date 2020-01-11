7c67c454cc821d6dd5a632cb6df995346a45a011
madhumita25 <madhumita.bharde@gmail.com>
2020-01-11 19:19:56 +0530

    All exercise completed.

---
+++ Lightbend-Java-Language-Futures/LJL-Futures-SNAPSHOT/exercises/src/main/java/com/lightbend/futures/CustomerService.java
@@ -0,0 +1,56 @@
+package com.lightbend.futures;
+
+import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
+class CustomerService {
+
+    private CustomerRepository customerRepository;
+
+    CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    CompletableFuture<UUID> addCustomer(String firstName, String lastName, String address, String phoneNumber) {
+        UUID customerId = UUID.randomUUID();
+        return customerRepository.saveCustomer(new Customer(
+            customerId,
+            firstName,
+            lastName,
+            address,
+            phoneNumber
+        )).thenApply(ignore -> customerId);
+    }
+
+    CompletableFuture<Optional<String>> getCustomerFirstName(UUID customerId) {
+        return customerRepository.getCustomer(customerId).thenApply(c -> c.map(Customer::getFirstName));
+    }
+
+    CompletableFuture<Optional<String>> getCustomerLastName(UUID customerId) {
+        return customerRepository.getCustomer(customerId).thenApply(c -> c.map(Customer::getLastName));
+    }
+
+    CompletableFuture<Optional<String>> getCustomerAddress(UUID customerId) {
+        return customerRepository.getCustomer(customerId).thenApply(c -> c.map(Customer::getAddress));
+    }
+
+    CompletableFuture<Optional<String>> getCustomerPhoneNumber(UUID customerId) {
+        return customerRepository.getCustomer(customerId).thenApply(c -> c.map(Customer::getPhoneNumber));
+    }
+
+    /*
+    With join statements
+        CompletableFuture<Optional<String>> getCustomerLastName(UUID customerId) {
+        return customerRepository.getCustomer(customerId).join().map(Customer::getLastName);
+    }
+
+    CompletableFuture<Optional<String>> getCustomerAddress(UUID customerId) {
+        return customerRepository.getCustomer(customerId).join().map(Customer::getAddress);
+    }
+
+    CompletableFuture<Optional<String>> getCustomerPhoneNumber(UUID customerId) {
+        return customerRepository.getCustomer(customerId).join().map(Customer::getPhoneNumber);
+    }
+     */
+}
